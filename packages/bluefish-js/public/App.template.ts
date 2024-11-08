5f9079a653ff384b29f587d327d8f545434f05de
LJ Patterson <ljewel@mit.edu>
2024-11-08 12:25:39 -0500

    wrote docs + fixed app templates

--- packages/bluefish-js/public/App.template.ts
+++ packages/bluefish-js/public/App.template.ts
@@ -1,7 +1,9 @@
-import { Bluefish, Text, render } from "../src";
+import { Text, render } from "../src";
 
 function App() {
-  return Bluefish(Text("Duplicate this file and name it `App.tsx` to use the dev playground!"));
+  return Text(
+    "Duplicate this file and name it `App.ts` to use the dev playground!"
+  );
 }
 
 render(App, document.getElementById("root")!);
--- packages/bluefish-solid/public/App.template.tsx
+++ packages/bluefish-solid/public/App.template.tsx
@@ -1,13 +1,13 @@
-// import "solid-devtools";
-
 import { type Component } from "solid-js";
-import { Bluefish } from "../src";
+import { Bluefish, Text } from "../src";
 
 const App: Component = () => {
   return (
     <>
       <Bluefish>
-        Duplicate this file and name it `App.tsx` to use the dev playground!
+        <Text>
+          Duplicate this file and name it `App.tsx` to use the dev playground!
+        </Text>
       </Bluefish>
     </>
   );
