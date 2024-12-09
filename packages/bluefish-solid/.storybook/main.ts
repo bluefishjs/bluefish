44c88924e4f47cba4adf96d392e5ce89816e1bb4
Josh Pollock <jopo@mit.edu>
2024-12-09 14:52:09 -0500

    [chore] update storybook and solid-js dependencies

--- packages/bluefish-solid/.storybook/main.ts
+++ packages/bluefish-solid/.storybook/main.ts
@@ -1,18 +1,23 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from "storybook-solidjs-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-docs",
-    "@chromatic-com/storybook"
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-docs"),
+    getAbsolutePath("@chromatic-com/storybook")
   ],
   framework: {
-    name: "storybook-solidjs-vite",
+    name: getAbsolutePath("storybook-solidjs-vite"),
     options: {},
   },
   docs: {},
 };
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
